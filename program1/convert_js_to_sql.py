f128a3a5ec42b25a8bf8b5a529f33b857c5de135
Alexande Krivosheev <alkrivosheev@gmail.com>
2020-08-23 16:10:21 +0300

    12

--- program1/convert_js_to_sql.py
+++ program1/convert_js_to_sql.py
@@ -12,11 +12,15 @@ con = mysql.connector.connect(
 )
 
 cursor = con.cursor()
+# query = cursor.execute("CREATE TABLE Dictionary(Expression varbinary(100) NOT NULL,  Definition varbinary(5000) NOT NULL)")
 cursor.execute("TRUNCATE TABLE new_base.Dictionary;")
 con.commit()
 for item in data:
-    print(item, "::",''.join(data[item]))
-    cursor.execute("INSERT INTO new_base.Dictionary (Expression, Definition) VALUES ('%s', '%s');" % (item, ''.join(data[item])))
-    con.commit()
+    # if item == 'field':
+        print(item, "::",''.join(data[item]))
+        q_text = "INSERT INTO new_base.Dictionary (Expression,Definition) VALUES ('%s','%s');" % (''.join(item).replace("'", " "), ''.join(data[item]).replace("'", " "))
+        cursor.execute(q_text)
+
+con.commit()
 cursor.close()
 con.close()
